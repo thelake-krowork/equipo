29ce15b7ab3f0fe5283bc9505023a70e1b590c36
Krowork <thelake.krowork@gmail.com>
2024-09-27 18:34:08 +0200

    Updated animacion.java in another branch

--- animacion.java
+++ animacion.java
@@ -1,6 +1,6 @@
 public class Animacion {
   public static void main (String[] args) {
     System.out.println("Vamos equipo!");
-    System.out.println("A ganar!")
+    System.out.println("A ganar campeones!")
   }
 }
